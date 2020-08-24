5d391616b39e2ee91ca92066ef023b32245d9cfb
sinkinben <sinkinben@outlook.com>
2020-08-24 17:36:13 +0800

    fix: update alarm UI

--- lib/myalarm.dart
+++ lib/myalarm.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'mytimer.dart';
 
 // TODO: This is an empty page.
 class ClockDate {
@@ -9,7 +10,9 @@ class ClockDate {
   var csec;
   bool state;
   var nowtime;
-  ClockDate(this.chour, this.cmin, this.csec, this.state);
+  var description;
+  ClockDate(this.chour, this.cmin, this.csec, this.state, this.description);
+  // ClockDate(this.chour, this.cmin, this.csec, this.state);
 }
 
 List<ClockDate> cdlist = [];
@@ -56,8 +59,8 @@ class MyAlarmPageState extends State<MyAlarm> {
                   builder: (context) => SecondScreen(),
                 )).then((data) {
               setState(() {
-                ClockDate backdata =
-                    new ClockDate(data.chour, data.cmin, data.csec, data.state);
+                ClockDate backdata = new ClockDate(data.chour, data.cmin,
+                    data.csec, data.state, data.description);
                 cdlist.add(backdata);
                 print(backdata.chour);
               });
@@ -110,7 +113,7 @@ class MyAlarmPageState extends State<MyAlarm> {
                 ),
                 Padding(padding: EdgeInsets.only(top: 2)),
                 Text(
-                  'this.data.msgContent',
+                  data.description,
                   maxLines: 1,
                   overflow: TextOverflow.ellipsis,
                   style: TextStyle(
@@ -137,59 +140,94 @@ class SecondScreen extends StatefulWidget {
 class SecondScreenState extends State<SecondScreen> {
   var selectTime = DateTime.now();
   ClockDate cd;
+  TextEditingController alarmNameController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     print('${this} hashCode=${this.hashCode}');
     cd = new ClockDate(
-        selectTime.hour, selectTime.minute, selectTime.second, true);
+        selectTime.hour, selectTime.minute, selectTime.second, true, '闹钟');
     return Scaffold(
       appBar: AppBar(
         title: Text('新建闹钟'),
       ),
       body: Center(
-        child: Column(children: <Widget>[
-          Text(
-            '${cd.chour}:${cd.cmin}:${cd.csec}',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          FlatButton(
-              onPressed: () {
-                DatePicker.showTimePicker(context,
-                    // 是否展示顶部操作按钮
-                    showTitleActions: true,
-                    // change事件
-                    onChanged: (date) {
-                  print('change $date');
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CustomTextContainer(
+                  label: 'HRS',
+                  value: cd.chour.toString().padLeft(2, '0'), //padLeft 补占位符
+                ),
+                CustomTextContainer(
+                  label: 'MIN',
+                  value: cd.cmin.toString().padLeft(2, '0'),
+                ),
+                CustomTextContainer(
+                  label: 'SEC',
+                  value: cd.csec.toString().padLeft(2, '0'),
+                ),
+              ],
+            ),
+            // Text(
+            //   '${cd.chour}:${cd.cmin}:${cd.csec}',
+            //   style: Theme.of(context).textTheme.headline2,
+            // ),
+            FlatButton(
+                onPressed: () {
+                  DatePicker.showTimePicker(context,
+                      // 是否展示顶部操作按钮
+                      showTitleActions: true,
+                      // change事件
+                      onChanged: (date) {
+                    print('change $date');
+                  },
+                      // 确定事件
+                      onConfirm: (date) {
+                    print('confirm $date');
+                    setState(() {
+                      selectTime = date;
+                      cd.chour = selectTime.hour;
+                      cd.cmin = selectTime.minute;
+                      cd.csec = selectTime.second;
+                      cd.nowtime = selectTime.millisecondsSinceEpoch;
+                    });
+                  },
+                      // 当前时间
+                      // currentTime: DateTime(2019, 6, 20, 17, 30, 20), // 指定时间
+                      currentTime: DateTime.now(), // 当前时间
+                      // 语言
+                      locale: LocaleType.zh);
                 },
-                    // 确定事件
-                    onConfirm: (date) {
-                  print('confirm $date');
-                  setState(() {
-                    selectTime = date;
-                    cd.chour = selectTime.hour;
-                    cd.cmin = selectTime.minute;
-                    cd.csec = selectTime.second;
-                    cd.nowtime = selectTime.millisecondsSinceEpoch;
-                  });
-                },
-                    // 当前时间
-                    // currentTime: DateTime(2019, 6, 20, 17, 30, 20), // 指定时间
-                    currentTime: DateTime.now(), // 当前时间
-                    // 语言
-                    locale: LocaleType.zh);
-              },
-              child: Text(
-                '选择闹钟时间',
-                style: TextStyle(color: Colors.blue),
-              )),
-          RaisedButton(
-              child: Text("确定"),
-              onPressed: () {
-                ClockDate dat = cd;
-                Navigator.of(context).pop(dat);
-              }),
-        ]),
+                child: Text(
+                  '选择闹钟时间',
+                  style: TextStyle(color: Colors.blue),
+                )),
+            Container(
+              width: 250,
+              child: TextField(
+                controller: alarmNameController,
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(10.0),
+                  icon: Icon(Icons.add_alarm),
+                  labelText: "闹钟名称",
+                ),
+              ),
+            ),
+            RaisedButton(
+                child: Text("确定"),
+                onPressed: () {
+                  ClockDate dat = cd;
+                  dat.description = alarmNameController.text;
+                  Navigator.of(context).pop(dat);
+                }),
+          ],
+        ),
       ),
     );
   }
